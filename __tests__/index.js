0258c464044f14b92a061b5449d78ea9698f9d48
Bernd Matzner <bernd@matznermatzner.de>
2016-11-15 01:54:59 +0100

    Update rules and tests accordingly

--- __tests__/index.js
+++ __tests__/index.js
@@ -3,31 +3,73 @@ import stylelint from "stylelint"
 import test from "ava"
 
 const validCss = (
-`/** @define ComponentName */
+`/**
+ * Multi-line comment
+ */
 
-.ComponentName {
-  margin: 0;
+.selector-1,
+.selector-2,
+.selector-3[type="text"] {
+  display: block;
+  box-sizing: border-box;
+  background: linear-gradient(#fff, rgba(0, 0, 0, 0.8));
+  color: #333;
 }
 
-.ComponentName--modifierName {
-  margin: 0;
+.selector-a,
+.selector-b {
+  top: calc(calc(1em * 2) / 3);
+  padding: 10px !important;
 }
 
-.ComponentName-descendentName {
-  margin: 0;
+.selector-x { width: 10%; }
+.selector-y { width: 20%; }
+.selector-z { width: 30%; }
+
+/* Single-line comment */
+@media (min-width >= 60em) {
+  .selector {
+    transform: translate(1, 1) scale(3);
+  }
 }
 
-.ComponentName.is-stateOfComponent {
-  margin: 0;
+@media (min-orientation: portrait), projection and (color) {
+  .selector-i + .selector-ii {
+    background: color(rgb(0, 0, 0) lightness(50%));
+    font-family: helvetica, arial, sans-serif;
+  }
 }
 
+@media screen and screen and (min-resolution: 192dpi),
+  screen and (min-resolution: 2dppx) {
+  .selector {
+    margin: 10px;
+    margin-bottom: 5px;
+    height: 10rem;
+    background-image:
+      repeating-linear-gradient(
+        -45deg,
+        transparent,
+        #fff 25px,
+        rgba(255, 255, 255, 1) 50px
+      );
+    box-shadow:
+      0 1px 1px #000,
+      0 1px 0 #fff,
+      2px 2px 1px 1px #ccc inset;
+  }
+
+  .selector::after {
+    background-image: url("x.svg");
+    content: "→";
+  }
+}
 `)
 
 const invalidCss = (
 `a {
-  top: 0.2em;
+  top: .2em;
 }
-
 `)
 
 test("no warnings with valid css", t => {
@@ -38,7 +80,7 @@ test("no warnings with valid css", t => {
   .then(data => {
     const { errored, results } = data
     const { warnings } = results[0]
-    t.notOk(errored, "no errored")
+    t.falsy(errored, "no errored")
     t.is(warnings.length, 0, "flags no warnings")
   })
 })
@@ -51,8 +93,8 @@ test("a warning with invalid css", t => {
   .then(data => {
     const { errored, results } = data
     const { warnings } = results[0]
-    t.ok(errored, "errored")
+    t.truthy(errored, "errored")
     t.is(warnings.length, 1, "flags one warning")
-    t.is(warnings[0].text, "Unexpected leading zero (number-leading-zero)", "correct warning text")
+    t.is(warnings[0].text, "Expected a leading zero (number-leading-zero)", "correct warning text")
   })
 })
